26cb346ef86d8338892f64cb73777bc7f739c415
john <john.silvaromao@gmail.com>
2025-12-29 11:53:24 -0300

    Estudo de vetor. Programa que pega o nome, email e quarto desejado e faz um aluguel desse quarto em um vetor de 10 posições.

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+void main() {
+    Locale.setDefault(Locale.US);
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Quantos quartos seram alugados, de 0 á 9 ");
+    int n = scanner.nextInt();
+    scanner.nextLine();
+    Quartos[] quartos = new Quartos[10];
+
+    for (int i = 0; i < n; i++) {
+        System.out.println("Aluguel #" + (i + 1) + ":");
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Número do quarto: ");
+        int numeroQuarto = scanner.nextInt();
+        scanner.nextLine();
+        quartos[numeroQuarto] = new Quartos(nome, email);
+    }
+
+    System.out.println("Quartos alugados: ");
+    for (int i = 0; i < 10; i++) {
+        if (quartos[i] != null) {
+            System.out.println(i + ": " + quartos[i].nome + ", " + quartos[i].email);
+
+        }
+    }
+}
---
+++ src/Quartos.java
@@ -0,0 +1,10 @@
+public class Quartos {
+    public String nome;
+    public String email;
+
+
+    public Quartos(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+}
